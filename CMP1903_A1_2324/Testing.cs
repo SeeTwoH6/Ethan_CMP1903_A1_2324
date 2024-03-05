6dea74242ca7976693e482b795a430dc3d4e1be1
SeeTwoH6 <ethan.caballero@yahoo.com>
2024-03-05 21:35:13 +0000

    Push all code to forReview, ready for review

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
 
 namespace CMP1903_A1_2324
 {
@@ -19,5 +13,23 @@ namespace CMP1903_A1_2324
          */
 
         //Method
+        public void Check()
+        {
+            Console.WriteLine("Testing die class...");
+            Die testDice = new Die();
+            testDice.RollDice();
+            Debug.Assert(testDice.Roll > 0 && testDice.Roll < 7);
+            Console.WriteLine("Rolls are confirmed to be within [1,6]");
+
+            Console.WriteLine("Testing game class...");
+            Game testGame = new Game();
+            for (int i = 0; i < 5; i++)
+            {
+                testGame.CreateDice();
+            }
+            testGame.CalcSum();
+            Debug.Assert(testGame.PrintSum() == testGame.DieList[0] + testGame.DieList[1] + testGame.DieList[2] + testGame.DieList[3] + testGame.DieList[4]);
+            Console.WriteLine("The output of the PrintSum() method of the Game class is equal to the sum of the first die roll + ... + last die roll");
+        }
     }
 }
