306cea6eb1aa31f131289127fc2e7f02ef7cb53d
SeeTwoH6 <145765618+SeeTwoH6@users.noreply.github.com>
2024-03-05 21:56:08 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -1,14 +1,1 @@
-﻿using System;
 
-namespace CMP1903_A1_2324
-{
-    internal class Die
-    {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
-        //Property
-        //Method
-    }
-}
