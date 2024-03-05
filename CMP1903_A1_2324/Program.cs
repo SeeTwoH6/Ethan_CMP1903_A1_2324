a8c4a19fb3a14e97c3dffa8183a8689a5bb27aae
SeeTwoH6 <145765618+SeeTwoH6@users.noreply.github.com>
2024-03-05 21:56:27 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -1,15 +1,1 @@
-﻿using System;
 
-namespace CMP1903_A1_2324
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            /*
-             * Create a Game object and call its methods.
-             * Create a Testing object to verify the output and operation of the other classes.
-             */
-        }
-    }
-}
