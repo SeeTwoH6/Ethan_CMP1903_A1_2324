f199124fec865e2b807733e5acace86acee44ce9
SeeTwoH6 <ethan.caballero@yahoo.com>
2024-03-05 21:30:32 +0000

    Push Empty Code to Master

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -15,58 +15,6 @@ namespace CMP1903_A1_2324
          * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
-
-        //Variables
-        private int _Sum;
-        private List<int> _DieList = new List<int>(); //List required mainly for testing purposes as I need access to each dice roll from outside this class
-
-        //Properties
-        public int Sum
-        {
-            get { return _Sum; }
-            set { _Sum = value; }
-        }
-        public List<int> DieList
-        {
-            get { return _DieList; }
-            set { _DieList = value; }
-        }
-
         //Methods
-        //Creates an object of class Die, rolls it and adds it to a list
-        public void CreateDice()
-        {
-            Die dice = new Die();
-            DieList.Add(dice.RollDice());
-        }
-
-        //Goes through each item in the list and adds them together
-        public int CalcSum()
-        {
-            foreach (int dice in DieList)
-            {
-                Sum += dice;
-            }
-            return Sum;
-        }
-
-        //Prints sum of dice roll
-        //Created a separate method as calling CalcSum() multiple times incorrectly recalculates the sum of dice
-        public int PrintSum()
-        {
-            return Sum;
-        }
-
-        //Prints the information to the user of what each die rolled
-        public void PrintDice()
-        {
-            int count = 1;
-            foreach (int i in DieList)
-            {
-                Console.WriteLine($"Roll of die {count} is {i}");
-                count++;
-            }
-        }
-
     }
 }
